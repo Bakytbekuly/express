56d0b02931ab4dccda50c5bf92906db04b82b346
Rustem <mr.rustem071193@gmail.com>
2024-06-01 19:08:47 +0600

    /

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const express = require("express")
 const app = express()
 const port = 3020
 
-const user = {
-	name: "Rustem",
-}
+const user = [
+	{
+		id: 1,
+		name: "Leanne Graham",
+		username: "Bret",
+		email: "Sincere@april.biz",
+		address: {
+			street: "Kulas Light",
+			suite: "Apt. 556",
+			city: "Gwenborough",
+			zipcode: "92998-3874",
+			geo: {
+				lat: "-37.3159",
+				lng: "81.1496",
+			},
+		},
+		phone: "1-770-736-8031 x56442",
+		website: "hildegard.org",
+		company: {
+			name: "Romaguera-Crona",
+			catchPhrase: "Multi-layered client-server neural-net",
+			bs: "harness real-time e-markets",
+		},
+	},
+]
 
 app.get("/user", (req, res) => res.send(user))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
